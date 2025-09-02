13e347ab6278405acd912f8edb753621ba1fad1c
Gabriel Moretti <bielmoreti@gmail.com>
2025-09-02 19:41:59 -0300

    feat/fix: fixing env config and adding cache dependencies

--- backend/src/main/java/com/incidents/IncidentsApplication.java
+++ backend/src/main/java/com/incidents/IncidentsApplication.java
@@ -1,11 +1,25 @@
 package com.incidents;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class IncidentsApplication {
+
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+        
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+        
         SpringApplication.run(IncidentsApplication.class, args);
     }
 }
