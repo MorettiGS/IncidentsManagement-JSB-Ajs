2b6b1bf7d36586caa41fcb71532f82d1f3c19994
Gabriel Moretti <bielmoreti@gmail.com>
2025-09-02 18:43:07 -0300

    feat/fix: fixing authentication variable name and logger dependencies

--- backend/src/main/java/com/incidents/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/incidents/security/JwtAuthenticationFilter.java
@@ -43,7 +43,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 UsernamePasswordAuthenticationToken authenticationToken = 
                     new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(AuthenticationToken);
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
         }
         
